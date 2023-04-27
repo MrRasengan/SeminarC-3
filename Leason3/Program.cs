967200ef5acd1f17eceda81470b1d8e8690110d9
Konstantin <kostik-kostik-12@mail.ru>
2023-04-27 22:27:36 +0300

    add Leason3

---
+++ Leason3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+//A (3,6); B (2,1) -> 5,09 
+//A (7,-5); B (1,-1) -> 7,21
+
+double x1 = Prompt("Введите x1: ");
+double y1 = Prompt("Введите y1: ");
+double x2 = Prompt("Введите x2: ");
+double y2 = Prompt("Введите y2: ");
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    double coordNumber = double.Parse(Console.ReadLine()!);
+    return coordNumber;
+}
+Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2) ),2) );
